6fddffc542fe885b9672de63020aebba70cd9859
Bibek Shrestha <bibek.shrestha@metropolia.fi>
2022-10-26 11:28:12 +0300

    Week 1 - Exercises 1 and 2 done

---
+++ week1-node/script.js
@@ -0,0 +1,17 @@
+'use strict';
+
+// use const or let
+// never use 'var'
+const _ = require('lodash');
+
+//Exercise  1
+console.log("This is JavaScript");
+
+//Exercise 2
+let output = 'Just testing nodemon, using lodash to convert' +
+                ' this is camel case';
+
+console.log(output);
+
+output = _.camelCase(output);
+console.log(output);
