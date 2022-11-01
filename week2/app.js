5b75e721929d4f5ac829d0117009d4d572324598
Bibek Shrestha <bibek.shrestha@metropolia.fi>
2022-11-01 15:00:51 +0200

    added basic dummy routing

--- week2/app.js
+++ week2/app.js
@@ -4,7 +4,30 @@ const app = express();
 const port = 3000;
 
 app.get('/cat', (req, res) => {
+  //console.log(req)
   res.send('From this endpoint you can get cats.')
+});
+
+app.get('/cat/:catId', (req, res) => {
+  console.log(req.params)
+  res.send('From this endpoint you can get cat with Id: ' + req.params.catId)
+});
+
+app.post('/cat', (req, res) => {
+  console.log(req);
+  res.send('From this endpoint, you can add more cats.')
+})
+
+app.get('/users', (req, res) => {
+  res.send('From this endpoint you can get users.')
+});
+
+app.put('/cat', (req, res) => {
+  res.send('From this endpoint you can edit cats.')
+});
+
+app.delete('/cat', (req, res) => {
+  res.send('From this endpoint you can delete cats.')
 });
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
